69812a73ac837010ff5ef14984e4a38d59945517
Rhys Brame <rhysbrame@gmail.com>
2017-05-09 17:47:19 +0100

    created the dinosaur field offspring and tested

--- dinosaur.js
+++ dinosaur.js
@@ -1,7 +1,6 @@
 var Dinosaur = function(type, offspring){
   this.type = type;
   this.offspring = offspring;
-
 }
 
 module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -5,11 +5,15 @@ describe('Dinosaur', function(){
   var dinosaur;
 
   beforeEach(function(){
-    dinosaur = new Dinosaur('type1');
+    dinosaur = new Dinosaur('type1', 3);
   });
 
   it('should have a type', function(){
     assert.equal('type1', dinosaur.type);
   });
 
-}) ;
+  it('should have an offspring number', function(){
+    assert.equal(3, dinosaur.offspring)
+  });
+
+});
